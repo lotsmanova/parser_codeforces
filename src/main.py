6f12785ee35aa72e4fec6a3b4cfbbc773e9c0911
violetta <lotsmanovavioletta@gmail.com>
2023-11-13 22:02:38 +0300

    fixed main

--- src/main.py
+++ src/main.py
@@ -1,14 +1,8 @@
 import os
-import time
-
-from apscheduler.schedulers.background import BackgroundScheduler
-
 from src.getapi import CodeforcesAPI
 from src.dbworker import PostgresWorker
 from dotenv import load_dotenv
-
-from src.scheduler_task import job
-from src.utils import create_database
+from src.utils import create_database, check_create_db
 
 load_dotenv('../.env')
 
@@ -19,36 +13,33 @@ token_tg = os.getenv('TELEGRAM_TOKEN')
 
 def main():
     codeforces_api = CodeforcesAPI()
-    db_worker = PostgresWorker(db_name, db_password)
 
     # parser data in codeforces
     codeforces_data = codeforces_api.get_data()
+    print('Получены данные с сайта codeforces.com')
 
     # create db
-    if db_worker.check_create_db is False:
+    if check_create_db is False:
         create_database(db_name, db_password)
+        print(f'Создана база данных {db_name}')
+    else:
+        print(f"База данных {db_name} уже существует")
 
     # create table
+    db_worker = PostgresWorker(db_name, db_password)
+
     if db_worker.check_create_table() is False:
         db_worker.create_table()
+        print('Созданы таблицы topics и tasks')
+    else:
+        print("Таблицы topics и tasks уже существуют")
 
     # add data to db
     db_worker.add_data(codeforces_data)
+    print(f'Добавлены данные в БД {db_name}')
+
+    db_worker.end_connect()
 
 
 if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(job, 'interval', hours=1)
-    scheduler.start()
-    print('Нажмите Ctrl+{0}, чтобы завершить работу программы'.format('Break' if os.name == 'nt' else 'C'))
-
-    try:
-        while True:
-            time.sleep(1)
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
-
-
-
-
-
+    main()
