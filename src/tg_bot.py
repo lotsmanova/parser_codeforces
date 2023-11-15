4d136320b8ba387e0a1a770176cb6d973992848b
violetta <lotsmanovavioletta@gmail.com>
2023-11-15 13:28:30 +0300

    fixed bot4

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -1,3 +1,5 @@
+import sys
+
 import psycopg2
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -7,6 +9,7 @@ from src.main import token_tg, db_name, db_password
 
 bot = telebot.TeleBot(token_tg)
 
+# default values
 topic = ''
 rating = 0
 
@@ -22,6 +25,7 @@ for tuple_topic in list_topic_db:
     str_topic = ''.join(tuple_topic)
     list_str_topic.append(str_topic)
 
+
 @bot.message_handler(content_types=['text'])
 def start(message):
     """Функция привествия пользователя"""
@@ -31,51 +35,59 @@ def start(message):
             message.from_user.id,
             "Привет! Я бот для получения задач с сайта codeforces.com."
             "Чтобы получить задачи с сайта, следуй дальнейшим инструкциям."
+        )
+        bot.send_message(
+            message.from_user.id,
             f"Выбери уровень сложности от {min_rating} до {max_rating}"
         )
-    # elif message.text == '/stop':
-    #     bot.send_message(
-    #         message.from_user.id,
-    #         "Бот остановлен. Напиши '/start', если хочешь начать"
-    #     )
+        bot.register_next_step_handler(message, get_rating)
     else:
         bot.send_message(
             message.from_user.id,
-            "Напиши '/start'"
+            "Чтобы отправить запрос, напиши '/start'"
         )
 
-    bot.register_next_step_handler(message, get_rating)
+
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    sys.exit()
 
 
 def get_rating(message):
     """Функция выбора сложности задачи"""
 
     global rating
+    global message_user_id
 
-    if message.text.isdigit():
-        if min_rating < int(message.text) < max_rating:
-            rating = message.text
+    if message.text == 'stop':
+        stop(message)
+    else:
+        if message.text.isdigit():
+            if min_rating <= int(message.text) <= max_rating:
+                rating = message.text
 
-            main_kb = InlineKeyboardMarkup()
-            buttons = []
-            for topic_task in list_str_topic:
-                buttons.append(InlineKeyboardButton(topic_task, callback_data=topic_task))
+                main_kb = InlineKeyboardMarkup()
+                buttons = []
+                for topic_task in list_str_topic:
+                    buttons.append(InlineKeyboardButton(topic_task, callback_data=topic_task))
 
-            main_kb.add(*buttons)
+                main_kb.add(*buttons)
 
-            bot.send_message(message.from_user.id, 'Выбери одну из тем: \n', reply_markup=main_kb)
+                bot.send_message(message.from_user.id, 'Выбери одну из тем: \n', reply_markup=main_kb)
+
+            else:
+                bot.send_message(
+                    message.from_user.id,
+                    f"Сложность должна быть в диапазоне от {min_rating} до {max_rating}. Повторите запрос"
+                )
+                start(message)
 
         else:
             bot.send_message(
                 message.from_user.id,
-                f"Сложность должна быть в диапазоне от {min_rating} до {max_rating}. Повторите запрос. Напиши '/start'"
+                "Сложность должна быть числом. Повторите запрос"
             )
-
-    else:
-        bot.send_message(
-            message.from_user.id,
-            "Сложность должна быть числом. Повторите запрос. Напиши '/start'"
-        )
+            start(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -85,7 +97,9 @@ def callback(call):
     for topic_task in list_str_topic:
         if topic_task in call.data:
             bot.send_message(call.message.chat.id,
-                             f'Обрабатываю запрос на получение задач по теме "{topic}" со сложностью {rating}')
+                             f'Обрабатываю запрос на получение задач по теме "{topic}" со сложностью {rating}.')
+            bot.send_message(call.message.chat.id,
+                             f'Чтобы получить ответ на запрос, нажми "ok"')
             bot.register_next_step_handler(call.message, get_data_db)
 
 
@@ -107,7 +121,8 @@ def get_data_db(message):
         topic_id = cur.fetchone()
 
         if topic_id is None:
-            bot.send_message(message.from_user.id, f'Тема "{topic}" не найдена в базе данных')
+            bot.send_message(message_user_id, f'Тема "{topic}" не найдена в базе данных')
+            start(message)
 
         cur.execute(
             f"""
@@ -144,12 +159,11 @@ def get_data_db(message):
                                  f"{num_for_link}/{index}\n")
 
             bot.send_message(message.from_user.id, f'Задачи по вашему запросу: \n' + '\n'.join(tasks_str))
-            bot.send_message(message.from_user.id, 'Ваш запрос выполнен, чтобы отправить новый запрос, '
-                                                   'введите: "/start"')
+            bot.send_message(message.from_user.id, f'Чтобы отправить новый запрос, введите "/start"')
 
         else:
-            bot.send_message(message.from_user.id, 'Задач по вашему запросу не найдено, '
-                                                   'чтобы отправить новый запрос, введите: "/start"')
+            bot.send_message(message.from_user.id, 'Задач по вашему запросу не найдено. Повторите запрос')
+            start(message)
 
 
 bot.polling(none_stop=True, interval=0)
