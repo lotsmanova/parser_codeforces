3c77a20bd76dd93aea5a28c6966400cc830a867e
violetta <lotsmanovavioletta@gmail.com>
2023-11-15 14:37:39 +0300

    fixed bot5

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -1,22 +1,21 @@
 import sys
-
 import psycopg2
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 from src.dbworker import PostgresWorker
 from src.main import token_tg, db_name, db_password
 
 bot = telebot.TeleBot(token_tg)
 
-# default values
-topic = ''
-rating = 0
-
 db_worker = PostgresWorker(db_name, db_password)
 
 min_rating = db_worker.get_min_rating()
 max_rating = db_worker.get_max_rating()
+
+# default values
+topic = ''
+rating = min_rating
+running = True
 
 list_topic_db = db_worker.get_topic()
 list_str_topic = []
@@ -30,38 +29,46 @@ for tuple_topic in list_topic_db:
 def start(message):
     """Функция привествия пользователя"""
 
+    global running
     if message.text == '/start':
+        running = True
         bot.send_message(
             message.from_user.id,
-            "Привет! Я бот для получения задач с сайта codeforces.com."
-            "Чтобы получить задачи с сайта, следуй дальнейшим инструкциям."
+            "Привет! Я бот для получения задач с сайта codeforces.com"
+            "Чтобы получить задачи с сайта, следуй дальнейшим инструкциям.\nЧтобы остановить бота, введите '/stop'"
         )
         bot.send_message(
             message.from_user.id,
             f"Выбери уровень сложности от {min_rating} до {max_rating}"
         )
         bot.register_next_step_handler(message, get_rating)
+
+    elif message.text == '/stop':
+        stop(message)
+
     else:
         bot.send_message(
             message.from_user.id,
-            "Чтобы отправить запрос, напиши '/start'"
+            "Чтобы отправить запрос, введите '/start'"
         )
 
 
 @bot.message_handler(commands=['stop'])
 def stop(message):
-    sys.exit()
+    global running
+    running = False
+
+    bot.send_message(message.from_user.id, "Бот остановлен")
+    # sys.exit()
 
 
 def get_rating(message):
     """Функция выбора сложности задачи"""
 
     global rating
-    global message_user_id
 
-    if message.text == 'stop':
-        stop(message)
-    else:
+    if message.text != '/stop':
+
         if message.text.isdigit():
             if min_rating <= int(message.text) <= max_rating:
                 rating = message.text
@@ -88,18 +95,22 @@ def get_rating(message):
                 "Сложность должна быть числом. Повторите запрос"
             )
             start(message)
+    else:
+        stop(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
     global topic
+    if not running:
+        return
     topic = call.data
     for topic_task in list_str_topic:
         if topic_task in call.data:
             bot.send_message(call.message.chat.id,
-                             f'Обрабатываю запрос на получение задач по теме "{topic}" со сложностью {rating}.')
+                             f'Обрабатываю запрос на получение задач по теме "{topic}" со сложностью {rating}')
             bot.send_message(call.message.chat.id,
-                             f'Чтобы получить ответ на запрос, нажми "ok"')
+                             f'Чтобы получить ответ на запрос, введите "ok"')
             bot.register_next_step_handler(call.message, get_data_db)
 
 
@@ -109,6 +120,9 @@ def get_data_db(message):
 
     global rating
     global topic
+    if not running:
+        return
+
     conn = psycopg2.connect(dbname=db_name, password=db_password, user='postgres')
     with conn.cursor() as cur:
         cur.execute(
@@ -121,7 +135,7 @@ def get_data_db(message):
         topic_id = cur.fetchone()
 
         if topic_id is None:
-            bot.send_message(message_user_id, f'Тема "{topic}" не найдена в базе данных')
+            bot.send_message(message.from_user.id, f'Тема "{topic}" не найдена в базе данных')
             start(message)
 
         cur.execute(
@@ -154,9 +168,9 @@ def get_data_db(message):
                     num_for_link = task_num[:len(task_num) - 1]
                     index = task_num[-1:]
 
-                tasks_str.append(f"{i}. Номер задачи: {task_num}, название: {task_name}, количество решений: {count}, "
-                                 f"ссылка на задачу: https://codeforces.com/problemset/problem/"
-                                 f"{num_for_link}/{index}\n")
+                tasks_str.append(f"{i}. Номер задачи: {task_num};\nНазвание: {task_name};\n"
+                                 f"Количество решений: {count};\nСсылка на задачу: "
+                                 f"https://codeforces.com/problemset/problem/{num_for_link}/{index}\n")
 
             bot.send_message(message.from_user.id, f'Задачи по вашему запросу: \n' + '\n'.join(tasks_str))
             bot.send_message(message.from_user.id, f'Чтобы отправить новый запрос, введите "/start"')
@@ -166,4 +180,6 @@ def get_data_db(message):
             start(message)
 
 
-bot.polling(none_stop=True, interval=0)
+# bot.polling(none_stop=True, interval=0)
+while running:
+    bot.polling(none_stop=True, interval=0)
