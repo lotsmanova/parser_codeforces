90e0bf434e2af7ffeb52e57f6b8cb77bc0173fab
violetta <lotsmanovavioletta@gmail.com>
2023-11-21 20:26:04 +0300

    add config

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv('../.env')
+
+db_name = os.getenv('DB_NAME')
+db_password = os.getenv('DB_PASSWORD')
+db_user = os.getenv('DB_USER')
+token_tg = os.getenv('TELEGRAM_TOKEN')
+api_codeforces = os.getenv('API_CODEFORCES')
