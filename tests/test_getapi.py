13cb180692f59c889c0c9e8ed4997dbe3ad0adbe
violetta <lotsmanovavioletta@gmail.com>
2023-11-23 16:07:21 +0300

    fixed code get api

--- tests/test_getapi.py
+++ tests/test_getapi.py
@@ -1,13 +1,20 @@
 from src.getapi import CodeforcesAPI
 
 
-def test_get_data():
+def test_success_get_data():
+
+    api = CodeforcesAPI('https://codeforces.com/api/problemset.problems')
+
+    result = api.get_data()
+    # TestCase1 проверка получения данных по API
+    assert 'problems' in result
+
+
+def test_failed_get_data():
     # TestCase 1 проверка получения данных по API
 
-    codeforces1 = CodeforcesAPI('https://codeforces.com/api/test')
-    codeforces2 = CodeforcesAPI('https://codeforces.com/api/problemset.problems')
+    api = CodeforcesAPI('https://codeforces.com/api/test')
 
-    if 'problems' in codeforces2.get_data():
-        res = True
-        assert res is True
-    assert codeforces1.get_data() == 'Ошибка при получении данных'
+    result = api.get_data()
+    # TestCase2 проверка получения данных по API
+    assert result == 'Ошибка при получении данных'
